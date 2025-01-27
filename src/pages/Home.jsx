e3df44e6519a266dd0680e3c56e7f8586c4a926c
flowihs <zmulenko500@gmail.com>
2025-01-27 19:44:18 +0300

    update 1.0.0.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,11 +12,11 @@ export default function Home () {
                     <ul className="content-list">
                         <li className="content-list__item">
                             <h2 className="title-2">Frontend</h2>
-                            <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, TailwindCSS, StyledComponents</p>
+                            <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, SASS, NPM, BootStrap, TailwindCSS, Material UI</p>
                         </li>
                         <li className="content-list__item">
                             <h2 className="title-2">Backend</h2>
-                            <p>NodeJS, MySQL, PHP</p>
+                            <p>Java, Spring</p>
                         </li>
                     </ul>
                 </div>
